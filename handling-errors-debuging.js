92aa40ff2cd215d59e61f03243725f36d7b5216f
IRONVOLT <fam0106@gmail.com>
2021-12-13 21:36:05 +0200

    Error tast

--- handling-errors-debuging.js
+++ handling-errors-debuging.js
@@ -1,22 +1,44 @@
-let x = [];
-let running = false;
-
-function grow(){
-    x.push(new Array(100000000).join('x'));
-    if(running){
-        setTimeout(grow, 1000);
+//==============================
+// Custom error
+class newError extends Error {
+    constructor(message) {
+      super(message);
+      this.name = this.constructor.name;
     }
+  }
+  
+  class FieldValidationError extends newError { }
+  
+  class PropertyRequiredError extends FieldValidationError {
+    constructor(property) {
+      super("Property is empty: " + property);
+      this.property = property;
+    }
+  }
+  
+  console.log(new PropertyRequiredError("field").name ); // PropertyRequiredError
+//==============================
+// Memorry leak task
 
-    new Float64Array(Math.pow(2, 27))
+// let x = [];
+// let running = false;
 
-    document.getElementById('leak-button').click(()=>{
-        running = true;
-        let a;
-        a = new Float64Array(Math.pow(2, 27))
-        console.log(a)
-    });
+// function grow(){
+//     x.push(new Array(100000000).join('x'));
+//     if(running){
+//         setTimeout(grow, 1000);
+//     }
 
-    document.getElementById('stop-button').click(()=>{
-        running = false;
-    });
-}
+//     new Float64Array(Math.pow(2, 27))
+
+//     document.getElementById('leak-button').click(()=>{
+//         running = true;
+//         let a;
+//         a = new Float64Array(Math.pow(2, 27))
+//         console.log(a)
+//     });
+
+//     document.getElementById('stop-button').click(()=>{
+//         running = false;
+//     });
+// }
